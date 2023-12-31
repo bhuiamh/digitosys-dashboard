c6926f9ec4ffc90674a2a6b26b4b7b478b421d07
bhuiamhe <bhuiamh@gmail.com>
2023-12-31 22:06:25 +0600

    Final Deploy

--- src/app/customer/customerProfile/page.tsx
+++ src/app/customer/customerProfile/page.tsx
@@ -1,34 +1,52 @@
+"use client";
+
+import Image from "next/image";
+
 export default function CustomerProfile() {
-    return (
-      <div className="bg-white py-8 px-4 shadow-md rounded-lg">
-        <h2 className="text-2xl font-bold mb-4 text-center text-[#23a888]">
-          Payment History
-        </h2>
-  
-        <div className="flex flex-col items-center justify-center text-center">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="currentColor"
-            className="w-200 h-20 mx-auto mb-4 text-[#dc5777]"
-          >
-            <path d="M10.464 8.746c.227-.18.497-.311.786-.394v2.795a2.252 2.252 0 0 1-.786-.393c-.394-.313-.546-.681-.546-1.004 0-.323.152-.691.546-1.004ZM12.75 15.662v-2.824c.347.085.664.228.921.421.427.32.579.686.579.991 0 .305-.152.671-.579.991a2.534 2.534 0 0 1-.921.42Z" />
-            <path
-              fill-rule="evenodd"
-              d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25ZM12.75 6a.75.75 0 0 0-1.5 0v.816a3.836 3.836 0 0 0-1.72.756c-.712.566-1.112 1.35-1.112 2.178 0 .829.4 1.612 1.113 2.178.502.4 1.102.647 1.719.756v2.978a2.536 2.536 0 0 1-.921-.421l-.879-.66a.75.75 0 0 0-.9 1.2l.879.66c.533.4 1.169.645 1.821.75V18a.75.75 0 0 0 1.5 0v-.81a4.124 4.124 0 0 0 1.821-.749c.745-.559 1.179-1.344 1.179-2.191 0-.847-.434-1.632-1.179-2.191a4.122 4.122 0 0 0-1.821-.75V8.354c.29.082.559.213.786.393l.415.33a.75.75 0 0 0 .933-1.175l-.415-.33a3.836 3.836 0 0 0-1.719-.755V6Z"
-              clip-rule="evenodd"
-            />
-          </svg>
-  
-          <h3 className="text-xl font-semibold text-[#23a888]">
-            You do not have any payment history yet.
-          </h3>
-  
-          <button className="mt-4 px-4 py-2 bg-[#23a888] hover:bg-[#0c352b] text-white font-medium rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-#1e9c7e">
-            Start Shopping
-          </button>
+  return (
+    <div className="max-w-md mx-auto bg-white p-6 rounded-md shadow-md">
+      <div className="text-center text-[#dc5777] text-2xl font-bold mb-6">
+        User Information
+      </div>
+
+      <div className="flex items-center mb-4">
+        <div className="w-36 h-36 rounded mr-4">
+          <Image height={100} width={100}
+            src="https://www.svgrepo.com/show/384674/account-avatar-profile-user-11.svg"
+            alt=""
+          />{" "}
+        </div>
+        <div>
+          <h1 className="text-xl font-bold">Customer Name</h1>
+          <p className="text-gray-600">customer@customer.com</p>
         </div>
       </div>
-    );
-  }
-  
+
+      <div className="grid grid-cols-2 gap-4 mb-4">
+        <div className="pt-8">
+          <p className="font-semibold">Phone Number:</p>
+          <p>+1121212456</p>
+        </div>
+        <div>
+          <p className="font-semibold">Date of Birth:</p>
+          <p>6 January, 2023</p>
+        </div>
+        <div>
+          <p className="font-semibold">Gender:</p>
+          <p>Male</p>
+        </div>
+        <div>
+          <p className="font-semibold">Address:</p>
+          <p>Road#10, Mirpur#1, Dhaka-1216</p>
+        </div>
+      </div>
+
+      <button className="mb-4 bg-[#dc5777] hover:bg-[#6b192d] text-white px-4 py-2 rounded">
+        Edit
+      </button>
+      <button className="bg-[#23a888] hover:bg-[#125243] ml-4 text-white px-4 py-2 rounded">
+        Save
+      </button>
+    </div>
+  );
+}
